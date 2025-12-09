0f9ad5081e128864d86ae284418b371aa69ba63c
nathan-wallace <nathan.s.wallace@gmail.com>
2025-12-09 18:13:06 +0000

    Deploying to gh-pages from @ nathan-wallace/text-banner@1e9db8949fef1f885a83194870a2f876fe6ac46c 🚀

---
+++ assets/NewsBanner.stories-BhhPcoQI.js
@@ -0,0 +1,13 @@
+import{R as e}from"./index-CgfFrydU.js";import"./_commonjsHelpers-CqkleIqs.js";const p={title:"News Banner",parameters:{layout:"fullscreen"}},s={minHeight:"100vh",display:"flex",alignItems:"center",justifyContent:"center",padding:"48px",boxSizing:"border-box"},l={maxWidth:"960px",width:"100%",border:"15px solid linear-gradient(135deg, #0b1d36, #142f57)",borderRadius:"0px",padding:"28px",textAlign:"center"},o={boxSizing:"border-box",clear:"both",color:"rgb(255, 255, 255)",display:"block",fontFamily:'"Bebas Neue", sans-serif',fontFeatureSettings:'"kern"',hyphens:"none",margin:0,padding:"0.5rem 2rem",textTransform:"uppercase",background:"rgba(0, 0, 0, 0.2)",background:"linear-gradient(135deg, #0b1d36, #142f57)",backdropFilter:"blur(4px)",textGrow:"per-line scale"},i={marginTop:"16px",color:"#0b1d36",fontFamily:'"Bebas Neue", sans-serif',textTransform:"uppercase",textGrow:"per-line scale",padding:"0.5rem 2.5rem"},t=()=>e.createElement("div",{style:s},e.createElement("style",null,`
+      @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+    `),e.createElement("section",{style:l},e.createElement("h1",{style:o},"WHAT THEY ARE SAYING..."),e.createElement("h2",{style:i},"ABOUT HHS'S ACTIONS TO",e.createElement("br",null)," PROTECT PARENTS' RIGHTS ",e.createElement("br",null),"IN CHILDREN'S HEALTH DECISIONS")));t.__docgenInfo={description:"",methods:[],displayName:"Default"};var r,n,a;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`() => <div style={containerStyle}>
+    <style>{\`
+      @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+    \`}</style>
+    <section style={cardStyle}>
+      <h1 style={headingStyle}>WHAT THEY ARE SAYING...</h1>
+      <h2 style={subheadingStyle}>
+        ABOUT HHS'S ACTIONS TO<br /> PROTECT PARENTS' RIGHTS <br />IN CHILDREN'S HEALTH DECISIONS
+      </h2>
+    </section>
+  </div>`,...(a=(n=t.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const m=["Default"];export{t as Default,m as __namedExportsOrder,p as default};
